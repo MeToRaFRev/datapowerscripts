6c91ea32ecba811f3529c682ae06cded91c1155d
MeToRaFRev <54957948+MeToRaFRev@users.noreply.github.com>
2019-09-08 09:44:51 +0300

    Add files via upload

--- DPanda.bat
+++ DPanda.bat
@@ -1,4 +1,5 @@
 @echo off
+:STARTUP
 echo Cleaning all other dockers
 powershell -command docker rm -f $(docker ps -aq) >nul 2>nul
 cls
@@ -8,12 +9,24 @@ echo (Dpanda) Datapower + Dpanda Docker is starting up (PORTS: 22;5550;5554;8000
 echo.
 echo.
 docker run --rm -it -e DATAPOWER_ACCEPT_LICENSE=true -e DATAPOWER_INTERACTIVE=true -e DATAPOWER_WORKER_THREADS=4 -p 9090:9090 -p 9080:9080 -p 22:22 -p 8000-8010:8000-8010 -p 5550:5550 -p 5554:5554 -p 10000-10010:10000-10010 --name dpanda dorser/dpanda.ide
-docker rm -f dpanda >nul 2>nul
 cls
+if "%errorlevel%"=="127" (
+echo.
+echo.
+echo "(ERROR) Docker Daemon isnt running!"
+echo "(FIX) Start Docker for Windows and click any key to try again."
+echo.
+echo.
+ping -n 5 127.0.0.1 >nul 2>nul
+pause
+cls
+goto STARTUP
+)
 echo.
 echo.
 echo (Dpanda) Datapower + Dpanda Docker is DOWN.
 echo.
 echo.
 ping -n 5 127.0.0.1 >nul 2>nul
+pause
 exit
